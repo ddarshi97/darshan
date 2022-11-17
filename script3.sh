23511e78fdeb5f67fe71e04c0c00ab009556905a
Girish <harugeri.girish@gmail.com>
2022-11-17 14:52:31 +0000

    girish modify

--- script3.sh
+++ script3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ls > file1
-echo "The current files in the folder is bengaluru karnataka"
-cat file1
+echo "The current files in the folder is bengaluru karnataka "
+cat file1, file2,file3
 
